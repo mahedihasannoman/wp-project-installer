1e308d6e44f306c37905525880b047e3552ae113
mahedihasannoman <mahedihasannoman@gmail.com>
2022-10-31 20:27:32 +0600

    Several updates

--- install.sh
+++ install.sh
@@ -16,13 +16,14 @@ clear='\033[0m'
 # `-g` Fot Git repo
 # `-b` for backup db path
 # `-d` for project domain
-while getopts p:g:b:d:i: flag
+while getopts p:g:b:d:h: flag
 do
     case "${flag}" in
         p) project=${OPTARG};;
         g) gitrepo=${OPTARG};;
         b) backup_dbpath=${OPTARG};;
         d) projectdomain=${OPTARG};;
+        h) vhost=${OPTARG};;
     esac
 done
 
@@ -70,12 +71,15 @@ cd ${project}
 echo "Cloning project.."
 git clone ${gitrepo}
 echo "${green}Project has been cloned successfully${clear}"
-project_root=$(find * -type d -prune -exec ls -d {} \; |head -1)
+#project_root=$(find * -type d -prune -exec ls -d {} \; |head -1)
+project_root=$(ls -td * | head -1)
+
+source ~/.bash_profile
 
 # Find the WP root dir.
 if [ -f "${project}/${project_root}/wp-cli.phar" ]
 then
-    wp_root="${project}/${project_root}"
+  wp_root="${project}/${project_root}"
 else
 	wp_root="${project}/${project_root}/public"
 fi
@@ -95,7 +99,7 @@ then
 	if [ ! -z "${project}/${projectdomain}-db/$backup_sql" ]
 	then
 		echo "Creating Database..."
-		${mysql} --user="${dbuser}" --password="${dbpass}" --execute="DROP DATABASE ${dbname};CREATE DATABASE ${dbname};"
+		${mysql} --user="${dbuser}" --password="${dbpass}" --execute="DROP DATABASE IF EXISTS ${dbname};CREATE DATABASE ${dbname};"
 		echo "${green}Database has been creared!${clear}"
 		echo "Importing Database..."
 		${mysql} --user="${dbuser}" --password="${dbpass}" ${dbname} < ${project}/${projectdomain}-db/${backup_sql}
@@ -114,33 +118,62 @@ then
 	${php} ${wp_root}/wp-cli.phar --path="${wp_root}" config set DB_HOST "127.0.0.1"
 else
 	# wp config not exists
-	${php} ${wp_root}/wp-cli.phar --path="${wp_root}" config create --dbname=${dbname} --dbuser=${dbuser} --dbpass=${dbpass} --dbhost=127.0.0.1
+  echo "${warn}wp-config.php file is not exists. Trying to generate wp-config.php file${clear}"
+	${php} ${wp_root}/wp-cli.phar --path=${wp_root} config create --dbname=${dbname} --dbuser=${dbuser} --dbpass=${dbpass} --dbhost=127.0.0.1
+  
+  ${php} ${wp_root}/wp-cli.phar --path="${wp_root}" config set WP_ALLOW_MULTISITE "true" --raw
+  ${php} ${wp_root}/wp-cli.phar --path="${wp_root}" config set MULTISITE "true" --raw
+  ${php} ${wp_root}/wp-cli.phar --path="${wp_root}" config set SUBDOMAIN_INSTALL "false" --raw
+  ${php} ${wp_root}/wp-cli.phar --path="${wp_root}" config set DOMAIN_CURRENT_SITE "${projectdomain}.devlocal"
+  ${php} ${wp_root}/wp-cli.phar --path="${wp_root}" config set PATH_CURRENT_SITE '/'
+  ${php} ${wp_root}/wp-cli.phar --path="${wp_root}" config set SITE_ID_CURRENT_SITE '1'
+  ${php} ${wp_root}/wp-cli.phar --path="${wp_root}" config set BLOG_ID_CURRENT_SITE '1'
+  
 fi
 
 echo "Replaceing domain in database..."
 ${php} ${wp_root}/wp-cli.phar --path="${wp_root}" search-replace "www.${projectdomain}" "${projectdomain}.devlocal" --all-tables --report-changed-only --url="www.${projectdomain}"
 ${php} ${wp_root}/wp-cli.phar --path="${wp_root}" search-replace "https://${projectdomain}.devlocal" "http://${projectdomain}.devlocal" --all-tables --report-changed-only --url="https://${projectdomain}.devlocal"
 
-echo "Creating vhost..."
-vhost="<VirtualHost *:80>\n
-    \tServerAdmin m.hasan@agentur-loop.com\n
-    \tDocumentRoot \"${wp_root}\"\n
-    \tServerName ${projectdomain}.devlocal\n
-    \t<Directory \"${wp_root}\">\n
-        \t\tOptions All\n
-        \t\tAllowOverride All\n
-    \t</Directory>\n
-    \tErrorLog \"logs/${projectdomain}.devlocal-error_log\"\n
-    \tCustomLog \"logs/${projectdomain}.devlocal-access_log\" common\n
-</VirtualHost>"
+create_vhost() {
+  echo "Creating vhost..."
+  vhost="<VirtualHost *:80>\n
+      \tServerAdmin m.hasan@agentur-loop.com\n
+      \tDocumentRoot \"${wp_root}\"\n
+      \tServerName ${projectdomain}.devlocal\n
+      \t<Directory \"${wp_root}\">\n
+          \t\tOptions All\n
+          \t\tAllowOverride All\n
+      \t</Directory>\n
+      \tErrorLog \"logs/${projectdomain}.devlocal-error_log\"\n
+      \tCustomLog \"logs/${projectdomain}.devlocal-access_log\" common\n
+  </VirtualHost>"
 
-echo $vhost >> /Applications/MAMP/conf/apache/extra/httpd-vhosts.conf
+  echo $vhost >> ${vhost_config}
 
-host_content="# Added by project-installer\n127.0.0.1\t${projectdomain}.devlocal\n# End of section"
+  host_content="# Added by project-installer\n127.0.0.1\t${projectdomain}.devlocal\n# End of section"
 
-sudo -- sh -c "echo '$host_content' >> /etc/hosts";
-sudo -H -u root bash -c '/Applications/MAMP/bin/stopApache.sh'
-sudo -H -u root bash -c '/Applications/MAMP/bin/startApache.sh'
+  sudo -- sh -c "echo '$host_content' >> /etc/hosts";
+  sudo -H -u root bash -c "${stopApache}"
+  sudo -H -u root bash -c "${startApache}"
 
-echo "${green}vhost setup successful: http://${projectdomain}.devlocal/wp-admin/${clear}"
-echo "Thank you!";
+  echo "${green}vhost setup successful: http://${projectdomain}.devlocal/wp-admin/${clear}"
+}
+
+# Check if vhost status is not passed as argument
+if [ "$vhost" == "" ]; then
+  read -p "${warn}Do you want to setup vhost? <y/N>${clear}" prompt
+  if [[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "Yes" ]]
+  then
+    create_vhost
+  fi
+fi
+
+if [[ $vhost == "y" || $vhost == "Y" || $vhost == "yes" || $vhost == "Yes" ]]; then
+  create_vhost
+fi
+
+echo "${green}Project Setup has been Completed!${clear}";
+
+code ${project_root}
+exit
